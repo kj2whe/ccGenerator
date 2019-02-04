df73142a65b525eef1a84c4e3f18d087a98649e7
Jason <kj2whe@gmail.com>
2019-02-03 21:43:25 -0500

    Added Production Environment variable

--- src/app/Services/LuhnService/luhn.service.ts
+++ src/app/Services/LuhnService/luhn.service.ts
@@ -4,6 +4,8 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
 
+import { environment } from '../../../environments/environment';
+
 import { CardResponseObject } from '../../Models/CardResponseObject';
 
 const httpOptions = {
@@ -15,8 +17,8 @@ const httpOptions = {
 })
 export class LuhnService {
 
-  private luhnAPIUrl = 'https://luhnapi.azurewebsites.net/api/luhn/GenerateNumber';  // URL to web api
-  // private luhnAPIUrl = 'https://localhost:5001/api/';  // URL to web api
+  private luhnAPIUrl = environment.APIEndpoint;
+
   private cro: CardResponseObject;
 
   constructor(
