5b23fe130a29bcafc8bb40ee6efa4c6a5c56b91c
Jason <kj2whe@gmail.com>
2020-06-28 22:47:44 -0400

    Added support for IINMetaRange

--- src/app/Components/display-result/display-result.component.ts
+++ src/app/Components/display-result/display-result.component.ts
@@ -15,13 +15,14 @@ import { FormatTypeObject } from '../../Models/FormatTypeObject';
 })
 export class DisplayResultComponent implements OnInit {
   cc: CardResponseObject;
-  // currentDateTime: Date;
   fto: FormatTypeObject[];
   selectionForm: FormGroup;
   abbr: string;
   displayFormat: string;
   iinMetaRangeEnd: number;
   iinMetaRangeStart: number;
+  isIINMetaRangeChoiceAvailable: boolean;
+  iinMetaRangeChoice: number;
   iinRange: number[];
   issuer: string;
   lengthOfDigits: number[];
@@ -29,6 +30,8 @@ export class DisplayResultComponent implements OnInit {
   rangeChosen: string;
   lengthChosen: string;
   ccButtonDisabled: boolean;
+  iinMetaRangeText: string;
+  iinMetaRangeValue: number;
 
 
   constructor(
@@ -46,11 +49,20 @@ export class DisplayResultComponent implements OnInit {
   }
 
   generateNumber(event: any): void {
+
+    let tt;
+
+    if (this.rangeChosen === undefined && document.querySelector('input[name="selected_iinRange"]:checked').value === 'AAA') {
+      tt = (Math.floor(Math.random() * (this.iinMetaRangeEnd - this.iinMetaRangeStart + 1)) + this.iinMetaRangeStart).toString();
+    } else {
+      tt = this.rangeChosen;
+    }
+
     this.ccButtonDisabled = true;
     document.getElementById('generateNumber').classList.add('d-none');
     document.getElementById('progressSpinner').classList.remove('d-none');
     this.luhnService
-    .getRandomCard(this.creditCardFormatChosen, this.rangeChosen, this.lengthChosen)
+    .getRandomCard(this.creditCardFormatChosen, tt, this.lengthChosen)
     .subscribe(
       data => {
         this.cc = data;
@@ -84,6 +96,7 @@ export class DisplayResultComponent implements OnInit {
     this.rangeChosen = undefined;
     this.lengthChosen = undefined;
     this.ccButtonDisabled = true;
+    this.isIINMetaRangeChoiceAvailable = false;
 
     const t = this.fto.find(x => x.abbr === FormatChosen.target.value);
 
@@ -92,22 +105,31 @@ export class DisplayResultComponent implements OnInit {
      this.iinMetaRangeEnd = t.IINMetaRangeEnd;
      this.iinMetaRangeStart = t.IINMetaRangeStart;
      this.iinRange = t.IINRange;
+     this.issuer = t.Issuer;
+     this.lengthOfDigits = t.LengthOfDigits;
+
+
+     if (this.iinMetaRangeEnd > 0 && this.iinMetaRangeStart > 0) {
+      // tslint:disable-next-line: max-line-length
+      this.iinMetaRangeText = 'Do you want to begin your Credit Card number between ' + this.iinMetaRangeStart + ' to ' + this.iinMetaRangeEnd + '?';
+      this.isIINMetaRangeChoiceAvailable = true;
+     }
 
      if (t.IINRange.length === 1) {
       this.rangeChosen = t.IINRange[0].toString();
      }
 
-     this.issuer = t.Issuer;
-     this.lengthOfDigits = t.LengthOfDigits;
-
      if (t.LengthOfDigits.length === 1) {
       this.lengthChosen = t.LengthOfDigits[0].toString();
+     }
+
+     if (t.IINRange.length === 1 && t.LengthOfDigits.length === 1) {
+      this.ccButtonDisabled = false;
      }
   }
 
   iinRangeChosen(rangeChosen: any): void {
     this.rangeChosen = rangeChosen.target.value;
-
     this.ccButtonDisabled =  (this.lengthChosen ? false : true);
   }
 
@@ -115,6 +137,10 @@ export class DisplayResultComponent implements OnInit {
     this.lengthChosen = lengthChosen.target.value;
 
     this.ccButtonDisabled =  (this.rangeChosen ? false : true);
+  }
+
+  iinMetaRangeChosen(rangeChosen: any): void {
+    this.ccButtonDisabled = false;
   }
 
 
